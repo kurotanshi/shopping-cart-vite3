2b7df74cd328c87dc142dbe274711c9175838e76
Kuro Hsu <kurotanshi@gmail.com>
2023-03-16 18:29:41 +0800

    add modal for comments

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,21 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, reactive, computed } from 'vue';
 
 const list = ref([]);
+const commentLists = ref([]);
 
 fetch('/list.json')
   .then(d => d.json())
   .then(res => {
     list.value = res;
   });
+
+fetch('/comment.json')
+  .then(d => d.json())
+  .then(res => {
+    commentLists.value = res;
+  });
+
 </script>
 
 <template>
@@ -19,7 +27,8 @@ fetch('/list.json')
           <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="m-0 navbar-nav">
               <li class="nav-item inline">
-                <a class="p-4 no-underline inline-block nav-link active hover:(bg-gray-300)" aria-current="page" href="#">商品</a>
+                <a class="p-4 no-underline inline-block nav-link active hover:(bg-gray-300)" aria-current="page"
+                  href="#">商品</a>
               </li>
               <li class="nav-item inline">
                 <a class="p-4 no-underline inline-block nav-link hover:(bg-gray-300)" href="#">當日特價</a>
@@ -122,4 +131,29 @@ fetch('/list.json')
       </section>
     </section>
   </main>
+
+  <!-- modal -->
+  <div
+    v-show="false"
+    class="fixed bg-opacity-75 bg-slate-900 w-full h-full top-0 left-0">
+    <div class="relative bg-white w-2/3 h-[75vh] border-2 mx-auto top-[12vh] overflow-y-scroll p-4">
+      <h2 class="text-center mb-4">老大</h2>
+
+      <div class="flex justify-center w-full h-60 bg-no-repeat bg-top bg-contain"
+        style="background-image: url('images/cat001.jpg')">
+      </div>
+
+      <ul class="my-4 px-4">
+        <li 
+          class="block py-4 border-bottom text-xl leading-8">
+          <div><i class="fas fa-user"></i> <span class="ml-4">user123</span></div>
+          <div><i class="fas fa-star"></i> <span class="ml-4">5</span></div>
+          <div>
+            <i class="fas fa-comments"></i>
+            <div class="-mt-7 pl-10">這隻貓咪很可愛，而且很聽話，非常容易照顧。</div>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
